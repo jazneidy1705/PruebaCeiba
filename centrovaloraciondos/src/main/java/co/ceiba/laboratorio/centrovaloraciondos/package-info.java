cea2d86921257cce95a268d8d49b5f5cc9bc39e5
jazneidy1705 <41234641+jazneidy1705@users.noreply.github.com>
2018-07-14 21:29:07 +0200

    se elimina estructura inicial

--- centrovaloraciondos/src/main/java/co/ceiba/laboratorio/centrovaloraciondos/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Paquete que contiene los elementos del centrovaloraciondos.
- * 
- * @author Christian A. Candela
- * @author Grupo de Investigacion en Redes Informacion y Distribucion - GRID
- *
- */
-package co.ceiba.laboratorio.centrovaloraciondos;
